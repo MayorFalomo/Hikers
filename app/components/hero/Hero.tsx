5516e577bca2333758f2410f6d8bcd9e4edc7218
MayorFalomo <mayorfalomo@gmail.com>
2023-09-28 22:05:06 +0100

    updated responsiveness

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -1,50 +1,124 @@
 'use client'
-import React, {useRef, useState} from 'react'
+import React, {useRef, useState, useEffect} from 'react'
 import styles from './Hero.module.css'
 import { HiOutlineUserCircle } from 'react-icons/hi'
 import { BsArrowDown } from 'react-icons/bs'
 import { IoLogoInstagram} from 'react-icons/io'
 import { FaXTwitter } from 'react-icons/fa6'
-import { useInView, motion } from 'framer-motion' 
+import { useInView, motion, usePresence } from 'framer-motion' 
 import Picture from './bgImg.png'
 import Image from 'next/image'
 import {CgMenuMotion} from 'react-icons/cg'
 import {MdOutlineClose} from 'react-icons/md'
-import { useScroll, useSpring, useTransform, MotionValue} from "framer-motion";
+import {  useAnimate, stagger} from "framer-motion";
 
 type Props = {}
 
 const Hero = (props: Props) => {
 
   const list = ['Equipment', 'About Us', 'Blog' ]
-  const [isOpen, setIsOpen ]= useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
-  function DisplayText({ list }: any) {
-    return (
-      <div className={styles.text}>
-        {list.map((item: any, index: any) => (
-          <div key={index} className={styles.texts} >
-            {item}
-          </div>
-        ))}
-      </div>
-    )
-  }
+  
+    // return (
+    //   <ul className={styles.text}>
+    //     {list.map((item: any, index: any) => (
+    //       <li key={index} className={styles.hoverUnderlineAnimation }  >
+    //         {item}
+    //       </li>
+    //     ))}
+    //   </ul>
+    // )
+
+  function useMenuAnimation(isOpen: boolean) {
+    const [scope, animate] = useAnimate();
+
+
+    //   useEffect(() => {
+    //     const menuAnimations = isOpen
+    //       ? [
+    //           [
+    //             "nav",
+    //             { transform: "translateX(0%)" },
+    //             { ease: [0.08, 0.65, 0.53, 0.96], duration: 0.6 }
+    //           ],
+    //           [
+    //             "li",
+    //             { transform: "scale(1)", opacity: 1, filter: "blur(0px)" },
+    //             { delay: stagger(0.05), at: "-0.1" }
+    //           ]
+    //         ]
+    //       : [
+    //           [
+    //             "li",
+    //             { transform: "scale(0.5)", opacity: 0, filter: "blur(10px)" },
+    //             { delay: stagger(0.05, { from: "last" }), at: "<" }
+    //           ],
+    //           ["nav",{ease: [0.08, 0.65, 0.53, 0.96]}, { transform: "translateX(100%)" }, { at: "-0.1" }]
+    //         ];
+    //     animate([
+    //       ["path.top",{} ],["path.middle", { opacity: isOpen ? 0 : 1 }, { at: "<" }],[
+    //         "path.bottom",
+    //         { d: isOpen ? "M 3 2.5 L 17 16.346" : "M 2 16.346 L 20 16.346" },
+    //         { at: "<" }
+    //       ],
+    //       ...menuAnimations
+    //     ]);
+    //   }, [isOpen]);
+
+    //   return scope;
+    // }
+     
+
+     useEffect(() => {
+    const menuAnimations = isOpen
+      ? [
+          [
+            "nav",
+            { transform: "translateX(0%)" },
+            { ease: [0.08, 0.65, 0.53, 0.96], duration: 0.6 }
+          ],
+          [
+            "li",
+            { transform: "scale(1)", opacity: 1, filter: "blur(0px)" },
+            { delay: stagger(0.05), at: "-0.1" }
+          ]
+        ]
+      : [
+          [
+            "li",
+            { transform: "scale(0.5)", opacity: 0, filter: "blur(10px)" },
+            { delay: stagger(0.05, { from: "last" }), at: "<" }
+          ],
+          ["nav", { transform: "translateX(-100%)" }, { at: "-0.1" }]
+        ];
+
+    animate([
+      [
+        "path.top",{ } ],
+      ["path.middle", { opacity: isOpen ? 0 : 1 }, { at: "<" }],
+      ["path.bottom", { d: isOpen ? "M 3 2.5 L 17 16.346" : "M 2 16.346 L 20 16.346" },
+        { at: "<" }
+      ],
+      ...menuAnimations
+    ]);
+       
+  }, [isOpen]);
+
+  return scope;
+}
+  const scope = useMenuAnimation(isOpen);
+
+  // console.log(isOpen, "This is isOpen");
+  
+  // console.log(scope, "This is scoped");
+  
   
   const body = useRef(null)
 
-    function useParallax(value: MotionValue<number>, distance: number) {
-  return useTransform(value, [0, 1], [-distance, distance]);
-}
-
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({ target: ref });
-  const y = useParallax(scrollYProgress, 50);
-
-
   const isInView = useInView(body, {margin: "-10%" })
 
-  const animate = {
+  const animated = {
     initial: { y: "200%" },
     open: {
       y: "0%",
@@ -52,33 +126,50 @@ const Hero = (props: Props) => {
     }
   }
   
+  
+
   return (
-      <section  ref={body} >
+    <div className={styles.section} ref={body} >
+       <nav ref={scope} className={isOpen ? styles.navActive : styles.navFlex} >
+            <ul className={styles.listItem} >
+                <li className={styles.hoverUnderlineAnimation } >Equipment </li>
+                <li className={styles.hoverUnderlineAnimation } >About us </li>
+              <li className={styles.hoverUnderlineAnimation} >Blog </li>
+              </ul>
+        <li> {<HiOutlineUserCircle fontSize='30px' />} Account </li>
+      </nav>
+      <p className={styles.hamburger} >
+          {isOpen ?
+            <span onClick={() => setIsOpen(false)} className={styles.close} >{< MdOutlineClose fontSize={40} cursor='pointer' />} </span> :
+            <span onClick={() => setIsOpen(true)} className={styles.open} >{< CgMenuMotion fontSize={40} cursor='pointer' />} </span>
+          }</p>
       <div className={styles.sky} style={{backgroundImage: 'url(./bgImg.png)' }} >
         {/* <Image className={styles.image} style={{backgroundAttachment: 'fixed'}} src={Picture} layout='fill' objectFit='cover' alt='img' placeholder='blur' /> */}
-          <nav className={styles.nav} >
+          <div className={styles.nav} >
           <h2>Hikers</h2>
-          <div className={isOpen ? styles.navActive : styles.navFlex} >
+          <div className={styles.desktopNav} >
             <ul>
-              <li>{<DisplayText list={list}  />} </li>
-                {/* <li className={styles.hoverUnderlineAnimation } >Equipment </li>
+                <li className={styles.hoverUnderlineAnimation } >Equipment </li>
                 <li className={styles.hoverUnderlineAnimation } >About us </li>
-                <li className={styles.hoverUnderlineAnimation } >Blog </li> */}
-            </ul>
+              <li className={styles.hoverUnderlineAnimation} >Blog </li>
+              </ul>
             <p> {<HiOutlineUserCircle fontSize='30px' />} Account </p>
+          </div>
+          {/* <ul ref={scoped} className={isOpen ? styles.navActive : styles.navFlex} >
+              <li>{<DisplayText list={list}  />} </li>
+            <p> {<HiOutlineUserCircle fontSize='30px' />} Account </p>
+            </ul> */}
+         
+          
+        
             </div>
-          <p className={styles.hamburger} >
-            {isOpen ? <span onClick={() => setIsOpen(false)} className={styles.close} >{< MdOutlineClose fontSize={40} cursor='pointer' />} </span> :
-            <span onClick={() => setIsOpen(true)} >{< CgMenuMotion fontSize={40} cursor='pointer' />} </span>
-          }</p>
-            </nav>
         <div className={styles.overLay} > </div>
-          <div ref={ref} className={styles.heroText} >
-          <div className={styles.overflow} ><motion.p variants={animate} initial="initial" animate={isInView ? "open" : ""} className={styles.guide} > <span></span> A HIKING GUIDE </motion.p></div>
-          <motion.h1  variants={animate} initial="initial" animate={isInView ? "open" : "" } className={styles.h1} >
+          <div className={styles.heroText} >
+          <div className={styles.overflow} ><motion.p variants={animated} initial="initial" animate={isInView ? "open" : ""} className={styles.guide} > <span></span> A HIKING GUIDE </motion.p></div>
+          <motion.h1  variants={animated} initial="initial" animate={isInView ? "open" : "" } className={styles.h1} >
             Be Prepared For The Mountains And Beyond!
           </motion.h1>
-           <div className={styles.overflow} > <motion.p  variants={animate} initial="initial" animate={isInView ? "open" : "" } >Scroll down {<BsArrowDown fontSize='30px' />} </motion.p></div>
+           <div className={styles.overflow} > <motion.p  variants={animated} initial="initial" animate={isInView ? "open" : "" } >Scroll down {<BsArrowDown fontSize='30px' />} </motion.p></div>
           </div>
         <div className={styles.socials} >
           <p>Follow us </p>
@@ -87,7 +178,7 @@ const Hero = (props: Props) => {
         </div>
         </div>
         
-      </section>
+      </div>
   )
 }
 
--- app/components/level/Level.tsx
+++ app/components/level/Level.tsx
@@ -5,6 +5,7 @@ import Blur from '../Blur'
 import Picture from './01.png'
 import { useRef } from "react";
 import { motion, useScroll, useSpring, useTransform, MotionValue, useInView} from "framer-motion";
+import Image from 'next/image'
 
 
 type Props = {}
@@ -33,15 +34,17 @@ const Level = (props: Props) => {
 
 
     return (
-      <section ref={body} className={styles.section} >
-        <div className={styles.container} >
+      <section  className={styles.section} >
+        <div ref={body} className={styles.container} >
                 <p className={styles.started} ><span> </span> GET STARTED </p>
           <motion.h2  variants={animate} initial="initial" animate={isInView ? "open" : "" } >What level of hiker are you? </motion.h2>
           <motion.p  variants={animate} initial="initial" animate={isInView ? "open" : "" } > Determining what level of hiker you are can be an important tool when planning future hikes. This hiking level guide will help you plan hikes according to different hike ratings set by various websites like All Trails and Modern Hiker. What type of hiker are you – novice, moderate, advanced moderate, expert, or expert backpacker? </motion.p>
             <p className={styles.readMore} style={{ color: '#fbd784', cursor: 'pointer' }} > read more {<BsArrowRight />} </p>
                 <motion.span style={{y}} className={styles.number} >01 </motion.span>
         </div>
-        <Blur  ref={ref} src={Picture} />
+         <div ref={body} className={styles.imageContainer} >
+          <Image className={styles.image} src={Picture} fill alt='img' placeholder='blur' />
+        </div>
                 {/* <img src="./01.png" alt="img" /> */}
             </section>
   )
